73d69bc6491816cd5bbfca3b9f4ec17cd263800c
Ryan Gang <ryan-gg@outlook.com>
2023-10-09 21:11:57 +0530

    2nd stage

--- DOCS.md
+++ DOCS.md
@@ -1,3 +1,11 @@
-## Stage 1
+## Stage 1 : Bind to a port
 
 Task is to start a TCP server on port 4221.
+
+## Stage 2 : Respond with 200
+
+- Accept a TCP connection
+- Read data from the connection (we'll get to parsing it in later stages)
+- Respond with `HTTP/1.1 200 OK\r\n\r\n` (there are two `\r\n`s at the end)
+
+Ref : https://docs.python.org/3/library/socket.html#example
--- app/main.py
+++ app/main.py
@@ -5,8 +5,13 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=False)
+    conn, addr = server_socket.accept()  # wait for client
+    with conn:
+        print("Connected by", addr)
+        _ = conn.recv(1024)
+        response = "HTTP/1.1 200 OK\r\n\r\n"
+        conn.send(response.encode())
 
 
 if __name__ == "__main__":
